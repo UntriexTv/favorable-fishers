b6afe140c466e172de97200ed9deb1062f1d48c8
Isaac <caasitroplla@gmail.com>
2021-07-11 17:01:04 +0100

    Update table.py

--- Widgets/table.py
+++ Widgets/table.py
@@ -1,25 +1,18 @@
+# This creates a table that randomly says fail or sucsess
 from rich.console import Console
 from rich.table import Table
 import random
 
-class GenereteTable():
-    def __init__(self):
-        pass
+def generate_table() -> Table:
+    """Make a new table."""
+    table = Table()
+    table.add_column("ID")
+    table.add_column("Value")
+    table.add_column("Status")
 
-    def setup(self):
-        kk = Console.input("hello: ")
-        Console.print(kk)
-
-    def run(self):
-        """Make a new table."""
-        table = Table()
-        table.add_column("ID")
-        table.add_column("Value")
-        table.add_column("Status")
-
-        for row in range(random.randint(2, 6)):
-            value = random.random() * 100
-            table.add_row(
-                f"{row}", f"{value:3.2f}", "[red]ERROR" if value < 50 else "[green]SUCCESS"
-            )
-        return table
+    for row in range(random.randint(2, 6)):
+        value = random.random() * 100
+        table.add_row(
+            f"{row}", f"{value:3.2f}", "[red]ERROR" if value < 50 else "[green]SUCCESS"
+        )
+    return table
