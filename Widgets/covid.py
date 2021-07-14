2c9ec106909826ee4b1804a58a46ac1c600d3ccc
Isaac <caasitroplla@gmail.com>
2021-07-14 20:06:55 +0100

    Update covid.py

--- Widgets/covid.py
+++ Widgets/covid.py
@@ -52,34 +52,6 @@ class covidView(Frame):
 
         return data[len(data)-1]
 
-'''
-def demo(screen, scene):
-    # define the size of each frame
-    height = 15
-    width = 50
-
-    # make all the scenes
-    scenes = [
-        Scene([
-            covidView(screen, height, width, 0, 0), 
-            covidView(screen, height, width, 0, 1), 
-            covidView(screen, height, width, 1, 0),
-            covidView(screen, height, width, 1, 1)
-            ], -1, name='Main'),
-    ]
-
-    # running all the scenes
-    screen.play(scenes, stop_on_resize=True, start_scene=scenes[0], allow_int=True)
-
-
-last_scene = None
-while True:
-    try:
-        Screen.wrapper(demo, catch_interrupt=True, arguments=[last_scene])
-        sys.exit(0)
-    except ResizeScreenError as e:
-        last_scene = e.scene
-'''
 
 
 
