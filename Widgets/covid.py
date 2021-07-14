7029e576178c8ba3667943d1c9de60a81987ff2d
Isaac <caasitroplla@gmail.com>
2021-07-14 20:17:12 +0100

    Update covid.py

--- Widgets/covid.py
+++ Widgets/covid.py
@@ -11,6 +11,7 @@ class covidView(Frame):
     def __init__(self, screen, height, width, x, y, can_scroll=False):
         super(covidView, self).__init__(screen, height, width, x=x, y=y, can_scroll=can_scroll)
         self.needs_refresh = False
+        self.settings_open = False
 
         # create the layout
         layout = Layout([100], fill_frame=True)
@@ -25,6 +26,10 @@ class covidView(Frame):
         layout.add_widget(Label('Active:    ' + str(self.json_data['Active'])))
         layout.add_widget(Label('Date:      ' + str(self.json_data['Date'])))
 
+        if self.settings_open == True:
+            layout.add_widget(Text('Country:', 'country'))
+
+
         # creating the bottom bar of widgets
         buttonBar = Layout([1, 1, 1, 1, 1])
         self.add_layout(buttonBar)
@@ -36,11 +41,12 @@ class covidView(Frame):
     def _quit():
         raise StopApplication('User pressed quit')
 
-    @staticmethod
-    def _config():
-        # this function is to configure the widget i.e. change the country 
-        # not done yet obvs
-        pass
+    def _config(self):
+        if self.settings_open == True:
+            self.settings_open = False
+        else:
+            self.settings_open = True
+
 
     def fetch_data(self):
         # getting the json data from the covid api and return the latest data point
@@ -51,8 +57,3 @@ class covidView(Frame):
         data = response.json()
 
         return data[len(data)-1]
-
-
-
-
-
